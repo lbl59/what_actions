8cca8aaa3844eb9cc9d4e59116fc3fba853966e7
Lillian Lau <71844489+lbl59@users.noreply.github.com>
2021-05-31 21:12:04 -0400

    Add files via upload

--- rof_dynamics.py
+++ rof_dynamics.py
@@ -10,7 +10,6 @@ sns.set_theme()
 sns.set_style("white")
 # Get the ROF tables ################################################
 rf_data = []
-st_data = []
 for each_file in glob.glob('C:/Users/lilli/Desktop/Risk-of-Failure/dynamics/restr_freq/restr_freq_*.csv'):
     rf_data.append(each_file)
 # Get cwd code thanks to https://thispointer.com/python-how-to-get-the-current-working-directory/
@@ -18,7 +17,6 @@ for each_file in glob.glob('C:/Users/lilli/Desktop/Risk-of-Failure/dynamics/rest
 cwd = os.getcwd()
 rf_filename = cwd + "/dynamics/restr_freq/restr_freq_1.0_0.05.csv"
 rof_filename = cwd + "/dynamics/short_term_risk/risk_1.0_0.05.csv"
-
 restr_freq = np.loadtxt(rf_filename, delimiter = ",")
 rof = np.loadtxt(rof_filename, delimiter=",")
 years= np.arange(0,2340,1)
@@ -35,9 +33,12 @@ for i in range(len(alpha)):
     first_restriction = np.nonzero(rf_alpha_r)
     restriction_year[i] =years[first_restriction][0]
 
-storage_filename = cwd + "/dynamics/str_dynamics/str_dynamics_1.0_0.05.csv"
-storage = np.loadtxt(storage_filename, delimiter = ",")
-pc_storage = (storage[r,:]/(14900*0.5))*100
+inflow_file = cwd + "\water_balance_files\jordan_lake_inflows.csv"
+demand_file = cwd + "\water_balance_files\cary_demand.csv"
+
+inflows = 0.20*np.loadtxt(inflow_file, delimiter=",")
+demand = 1.1*np.loadtxt(demand_file, delimiter=",")
+sp = inflows.shape[1] - demand.shape[1] + 52
 
 fig, ax = plt.subplots(figsize=(8,4))
 y_pos = np.arange(len(alpha))
@@ -52,10 +53,10 @@ ax.set_ylabel(r"$\alpha$ (%)")
 ax.set_title('First water restriction implementation')
 
 ax2=ax.twinx()
-ax2.plot(weeks, pc_storage[:90], color="royalblue", label="% Storage", linewidth=1.2)
-ax2.set_ylabel("% Storage")
+ax2.plot(weeks, inflows[0,sp:sp+90], label="Inflow (BG)", linewidth=1.2)
+ax2.set_ylabel("Inflow (BG)")
 
 handles, labels = [(a + b) for a, b in zip(ax.get_legend_handles_labels(), ax2.get_legend_handles_labels())]
 plt.legend(handles, labels, loc = "lower right", fontsize=12)
-plt.savefig("Figures/first_restriction_r0.png")
+#plt.savefig("Figures/first_restriction_r0.png")
 plt.show()
